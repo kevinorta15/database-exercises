3f27bcae9b2fbad824bab9cf4b742977a07475fe
Kevin Orta <orta.kevin@gmail.com>
2021-11-29 09:17:53 -0600

    completed database design

---
+++ database_design_lecture.sql
@@ -0,0 +1,64 @@
+use quasar_example_db;
+#
+CREATE TABLE IF NOT EXISTS authors(
+                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                         first_name VARCHAR(50),
+                         last_name  VARCHAR(100) NOT NULL,
+                         PRIMARY KEY (id)
+);
+
+INSERT INTO authors(first_name, last_name) VALUES
+                                               ('Douglas', 'Adams'),
+                                               ('Mark', 'Twain'),
+                                               ('Kurt', 'Vonnegut');
+#
+CREATE TABLE IF NOT EXISTS quotes (
+                          id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                          content TEXT NOT NULL,
+                          author_id INT UNSIGNED NOT NULL,
+                          PRIMARY KEY (id),
+                          FOREIGN KEY (author_id) REFERENCES authors (id)
+  );
+
+# Seeds
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'I love deadlines. I love the whooshing noise they make as they go by.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'Time is an illusion. Lunchtime doubly so.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Mark' and last_name = 'Twain'),
+        'Clothes make the man. Naked people have little or no influence on society.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Kurt' and last_name = 'Vonnegut'),
+        'The universe is a big place, perhaps the biggest.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'), 'Don''t Panic.');
+
+CREATE TABLE IF NOT EXISTS topics(
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        name VARCHAR(255),
+                        PRIMARY KEY (id)
+);
+
+INSERT INTO topics(name) VALUES
+                             ('Space and Time'),
+                             ('Humor'),
+                             ('Office Life'),
+                             ('Hitchiker''s Guide to the Galaxy');
+
+CREATE TABLE IF NOT EXISTS quote_topic (
+                             quote_id INTEGER UNSIGNED NOT NULL,
+                             topic_id INTEGER UNSIGNED NOT NULL,
+                             FOREIGN KEY (quote_id) REFERENCES quotes(id),
+                             FOREIGN KEY (topic_id) REFERENCES topics(id)
+);
+
+INSERT INTO quote_topic(quote_id, topic_id)
+VALUES (1, 2), (1, 3), (2, 4), (3, 1),
+       (3, 2), (3, 3), (3, 4), (4, 2), (5, 1);
+
+select * from quotes
+    join authors a on a.id = quotes.author_id
+    join quote_topic qt on quotes.id = qt.quote_id
--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -21,4 +21,12 @@ values ('K-Dawg', 'Kdawg@example.com',(select id from roles where name ='comment
 */
 
 SELECT first_name, last_name
-from employees where from_date IN (emp_no = '101010')
+from employees where hire_date IN (SELECT hire_date from employees WHERE emp_no = '101010');
+
+SELECT title from titles where emp_no IN(select emp_no from employees where first_name = 'Aamod');
+
+SELECT first_name, last_name from employees where emp_no IN(select emp_no from dept_manager where gender IN(select gender from employees where gender = 'f') AND to_date = '9999-01-01');
+
+SELECT dept_name from departments where dept_no IN(select dept_no from dept_manager where emp_no IN(select emp_no from employees where gender = 'f' AND to_date = '9999-01-01'));
+
+SELECT first_name, last_name from employees where emp_no IN(select emp_no from salaries where salary IN(select MAX(salary) from salaries ));
